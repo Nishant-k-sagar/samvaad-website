14c874070a0e345c08cbe4090a77c0e5a65f9ed8
Nishant-k-sagar <nishantsagar004@gmail.com>
2025-06-07 07:07:19 +0530

    added CSS file

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { RouterProvider } from 'react-router-dom';
 import router from './router';
+import './App.css';
 
 const App: React.FC = () => {
   return <RouterProvider router={router} />;
